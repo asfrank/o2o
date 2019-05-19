7dcdf07548cadc0138805ad8a1123c6332045255
asfrank <asfrank@qq.com>
2019-05-19 17:10:33 +0800

    图片处理，封装util

---
+++ src/main/java/com/imooc/o2o/util/ImageUtil.java
@@ -0,0 +1,68 @@
+package com.imooc.o2o.util;
+
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class ImageUtil {
+    private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    private static final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+
+    public static String generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr){
+        String realFileName = getRandomFileName();
+        String extension = getFileExtension(thumbnail);
+        makeDirPath(targetAddr);
+        String relativeAddr = targetAddr + realFileName + extension;
+        File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+        try{
+            Thumbnails.of(thumbnail.getInputStream()).size(200, 200)
+                    .watermark(Positions.BOTTOM_RIGHT,
+                            ImageIO.read(new File(basePath + "/watermark.png")),0.25f)
+                    .outputQuality(0.8f).toFile(dest);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return relativeAddr;
+    }
+
+    //创建目标路径所涉及的目录
+    private static void makeDirPath(String targetAddr) {
+        String realFileParentPath = PathUtil.getImgBasePath() + targetAddr;
+        File dirPath = new File(realFileParentPath);
+        if (!dirPath.exists()) {
+            dirPath.mkdirs();
+        }
+
+    }
+
+    //获取输入文件流的扩展名
+    private static String getFileExtension(CommonsMultipartFile cFile) {
+        String originalFileName = cFile.getOriginalFilename();
+        return originalFileName.substring(originalFileName.lastIndexOf("."));
+    }
+
+    //生成随机文件名，当前年月日小时分钟秒钟+五位随机数
+    private static String getRandomFileName() {
+        //获取随机的五位数
+        int rannum = r.nextInt(89999) + 10000;
+        String nowTimeStr = sDateFormat.format(new Date());
+        return nowTimeStr + rannum;
+    }
+
+
+//    public static void main(String[] args) throws IOException {
+//
+//        Thumbnails.of(new File("/Users/mac/Documents/xiaohuangren.jpg"))
+//                .size(200,200).watermark(Positions.BOTTOM_RIGHT,
+//                ImageIO.read(new File(basePath + "/watermark.png")), 0.25f).outputQuality(0.8f)
+//                .toFile("/Users/mac/Documents/xiaohuangrennew.jpg");
+//    }
+}
---
+++ src/main/java/com/imooc/o2o/util/PathUtil.java
@@ -0,0 +1,21 @@
+package com.imooc.o2o.util;
+
+public class PathUtil {
+    private static String separator = System.getProperty("file.separator");
+    public static String getImgBasePath(){
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if (os.toLowerCase().startsWith("win")){
+            basePath = "D:/projectdev/image";
+        }else {
+            basePath = "/Users/mac/Documents";
+        }
+        basePath = basePath.replace("/", separator);
+        return basePath;
+    }
+
+    public static String getShopImagePath(Long shopId){
+        String imagePath = "/upload/item/shop/" + shopId + "/";
+        return imagePath.replace("/", separator);
+    }
+}
